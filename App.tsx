2f705d49bac9ff1f7c5a79290a689e2aa046f953
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2024-03-26 21:22:26 -0300

    add mapping to imports

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import { ThemeProvider } from "styled-components/native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-
-import theme from "./src/theme";
-import SignIn from "./src/screens/SignIn";
-import { Loading } from "./src/components/Loading";
 import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+
+import theme from "@theme/index";
+import SignIn from "@screens/SignIn";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
